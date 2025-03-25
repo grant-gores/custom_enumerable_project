77d87fd47297fe723f77072ac2244159424026d6
grant-gores <140012939+grant-gores@users.noreply.github.com>
2025-03-25 14:43:10 -0500

    Set up project and send over code from replit.
    Do not have abilty to put ruby on one of my
    computers. Completed some custom enumberable methods
    and commented them in lib/my_enumerables.rb.

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -9,3 +9,66 @@ end
 class Array
   # Define my_each here
 end
+
+# class Array
+#   def my_each
+#       for item in self
+#           yield(item)
+#       end
+#   end
+  
+#   def my_map
+#       new_array = []
+#       for item in self
+#           new_array << yield(item)
+#       end
+#       new_array
+#   end
+  
+#   def my_any?
+#       for item in self
+#           return true if yield(item)
+#       end
+#       false
+#   end
+  
+#   def my_all?
+#       for item in self
+#           return false if yield(item)
+#       end
+#       true
+#   end
+  
+#   def my_select
+#       new_array = []
+#       for item in self
+#           if yield(item)
+#               new_array << item
+#           end
+#       end
+#       new_array
+#   end
+  
+# end
+
+
+# [1, 2, 3].my_each do |item|
+#   puts item + 10
+# end
+
+# [1, 2, 3].my_map do |item|
+#   puts item * 2
+# end
+
+# [1,2,3].my_any? do |item|
+# item == 1
+# end
+
+# [1,2,3].my_all? do |item|
+# item == 1
+# end
+
+# [1,2,3].my_select do |item|
+# item == 1
+# end
+
